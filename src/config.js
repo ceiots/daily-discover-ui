4b42f003d19b725616afeeacc700bcb859c468cb
ceiots <ceiots@126.com>
2025-01-17 20:33:25 +0800

    更新API基础URL，恢复为新的服务器地址

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
+//export const API_BASE_URL = 'http://localhost:8081/daily-discover';
 export const API_BASE_URL = 'https://48dd8861.r5.cpolar.top/daily-discover';
-//export const API_BASE_URL = 'https://0d79-120-229-23-170.ngrok-free.app/daily-discover';
