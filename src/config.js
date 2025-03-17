089fd90f94b4de9bdceee5eea7722c00e6c3d659
ceiots <ceiots@126.com>
2025-03-17 15:42:18 +0800

    更新API基础URL为新的HTTPS地址

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = 'http://localhost:8081/daily-discover';
-export const API_BASE_URL = 'http://618c0300.r5.cpolar.top/daily-discover';
+export const API_BASE_URL = 'https://5b1df3db.r5.cpolar.top/daily-discover';
